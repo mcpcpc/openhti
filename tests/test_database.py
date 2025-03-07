2b88ff0266321549f83ba682f859903e10476df7
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-03-07 17:06:47 -0500

    update

--- tests/test_database.py
+++ tests/test_database.py
@@ -31,7 +31,6 @@ class TestDatabase(IsolatedAsyncioTestCase):
             self.assertIsInstance(db1, Connection)
             db2 = get_db()
             self.assertIs(db1, db2)
-        self.assertIn("db", self.ctx.g) 
 
     @patch("openhti.database.connect")
     async def test_close_db(self, mock_connect):
