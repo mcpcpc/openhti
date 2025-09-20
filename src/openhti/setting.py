5966377f73b442d32df3cc84912e6d3a73e12dab
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-09-20 11:19:36 -0400

    update

--- src/openhti/setting.py
+++ src/openhti/setting.py
@@ -29,7 +29,8 @@ async def read() -> tuple:
     """Read settings callback."""
 
     settings = get_db().execute("SELECT * FROM setting").fetchall()
-    checksum = filter(lambda s: s["key"] == "checksum", settings)["value"]
+    func = lambda s: s["key"] == "checksum"
+    checksum = filter(func, settings)[0]["value"]
     dirty = checksum != get_checksum()
     print(dirty)
     return await render_template(
