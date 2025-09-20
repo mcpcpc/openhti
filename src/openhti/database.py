6b8ded85ad0fa24c5bdbe612e40c39478d5c87ab
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-09-20 09:24:23 -0400

    update

--- src/openhti/database.py
+++ src/openhti/database.py
@@ -85,12 +85,13 @@ def get_table_checksum(db, table_name):
     """
 
     rows = db.execute(
-        f"PRAGMA table_info({table_name})"
+        f"PRAGMA table_info(?)",
+        table_name
     ).fetchall()
     columns = [row[1] for row in rows]
     rows = db.execute(
        "SELECT * FROM ? ORDER BY ?",
-       [table_name, ', '.join(columns)]
+       (table_name, ', '.join(columns))
     ).fetchall()
     hasher = sha256()
     for row in rows:
