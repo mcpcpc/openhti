90a1866279dc438c647dcde037238826f09e60e7
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-09-21 15:58:39 -0400

    update

--- tests/test_database.py
+++ tests/test_database.py
@@ -78,6 +78,7 @@ class TestDatabase(IsolatedAsyncioTestCase):
     async def test_get_checksum(self):
         """Test that `get_checksum` returns the correct hashed SHA255 value."""
 
+        runner = self.app.test_cli_runner()
         runner.invoke(init_db_command)
         async with self.app.app_context():
             result = get_checksum()
